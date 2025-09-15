36614c28ed5ecc00f33c8ebf39f0c4be48b232d9
Pablo Emilio Escobar Gaviria <Heroin>
2025-09-15 12:50:24 +0200

    Talan jo

--- Heroin/src/App.vue
+++ Heroin/src/App.vue
@@ -1,20 +1,41 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/Welcome.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+  <header>
+    <a>
+      <img src="./assets/almaa.jpg" class="logo" alt="Gitkraken logo" />
     </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  </header>
+  <main></main>
+  <footer><h2>Zihájl</h2></footer>
 </template>
 
-<style scoped>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Bitcount+Grid+Double&display=swap');
+
+:root {
+  --bg-color: #1e1e2f;
+  --text-color: #c9d1d9;
+  --border-color: #303030;
+  --primary-color: #646cff;
+  --secondary-color: #42b883;
+  --font-family: 'Bitcount Grid Double', sans-serif;
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: 'Bitcount Grid Double', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #FFF;
+}
+
 .logo {
   height: 6em;
   padding: 1.5em;
---
+++ Heroin/src/components/Welcome.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Hello Vue!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyComponent'
+};
+</script>
+
+<style>
+/* Add your styles here */
+</style>
